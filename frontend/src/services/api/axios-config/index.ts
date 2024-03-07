b1f2d73532618a5fcb577ca842d14706972c8f06
André Brandão <andrebrandao12@yahoo.com>
2024-03-07 08:19:55 -0300

    Environment Variables Setup

--- frontend/src/services/api/axios-config/index.ts
+++ frontend/src/services/api/axios-config/index.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { errorInterceptor, responseInterceptor } from "./interceptors";
-import { error } from "console";
+import { Environment } from "environment";
 
 const Api = axios.create({
-    baseURL: 'http://localhost/8080'
+    baseURL: Environment.URL_BASE,
 })
 
 Api.interceptors.response.use(
